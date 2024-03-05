2e86fad825c7ff0734f29358c89ef089cd9ca953
D_Zucho <danny.nico.dev@outlook.com>
2024-03-05 07:33:23 -0800

    add layout and initial desktop header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,29 @@
+import { Link } from 'react-router-dom'
+import { Button } from './ui/button'
+
 const Header = () => {
   return (
     <header className='border-b-2 border-b-orange-500 py-6'>
-      <div className='container mx-auto flex justify-between items-center'></div>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link
+          to='/'
+          className='text-2xl lg:text-3xl font-bold text-orange-500 tracking-tight'
+        >
+          MernEats.com
+        </Link>
+
+        <div className='flex gap-4'>
+          <Button
+            asChild
+            className='bg-orange-500 hover:bg-orange-400 text-white'
+          >
+            <Link to='/'>Sign Up</Link>
+          </Button>
+          <Button asChild variant={'outline'}>
+            <Link to=''>Log In</Link>
+          </Button>
+        </div>
+      </div>
     </header>
   )
 }
