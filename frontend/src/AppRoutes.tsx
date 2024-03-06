f78dad7cd46e480346501cb4eba11bdec25e18f4
D_Zucho <danny.nico.dev@outlook.com>
2024-03-06 08:26:39 -0800

    add access token to create user functionality

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import HomePage from './pages/HomePage'
+import AuthCallbackPage from './pages/AuthCallbackPage'
 
 const AppRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route path='/auth-callback' element={<AuthCallbackPage />} />
       <Route path='/user-profile' element={<span>User Profile Page</span>} />
       <Route path='*' element={<Navigate to='/' />} />
     </Routes>
